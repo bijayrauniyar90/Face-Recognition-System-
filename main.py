1518b69be615e72a468b5de70593c2f0454739c6
Bijay Rauniyar <bijayrauniyar90@gmail,com>
2024-03-09 22:32:13 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -11,6 +11,118 @@ class Face_Recognition_System:
 
 
 
+        #Top img 
+        img = Image.open(r"C:\Users\KIIT01\Face Recognition System\images\img11.jpg")
+        img=img.resize((1700,130),Image.BILINEAR)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=1700,height=130)
+
+        #bg img
+        img3 = Image.open(r"C:\Users\KIIT01\Face Recognition System\images\bg.jpg")
+        img3=img3.resize((1530,710),Image.BILINEAR)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+ 
+        bg_img=Label(self.root,image=self.photoimg3)        
+        bg_img.place (x=0,y=130,width=1530,height=710)
+                       
+        title_lbl=Label(bg_img,text="CHECKING SYSTEM COMPABILITY INSIGHTS",font=("20th Century Font",30),bg="white",fg="darkgreen")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
+
+
+        #Student Details 
+        img4=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\stu.png")
+        img4=img4.resize((200,200),Image.BILINEAR)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img,image=self.photoimg4,cursor="hand2")
+        b1.place(x=200,y=100,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Student Details",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=300,width=200,height=40)
+
+        #Face Detector
+        img5=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\stu.jpg")
+        img5=img5.resize((200,200),Image.BILINEAR)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5,cursor="hand2")
+        b1.place(x=500,y=100,width=200,height=200)
+ 
+        b1_1=Button(bg_img,text="Face Detector",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=300,width=200,height=40)
+       
+       
+        # Attendance
+        img6=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\det.jpg")
+        img6=img6.resize((200,200),Image.BILINEAR)
+        self.photoimg6=ImageTk.PhotoImage(img6) 
+
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1.place(x=800,y=100,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=300,width=200,height=40)
+
+        # Help Desk
+        img7=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\help.jpg")
+        img7=img7.resize((200,200),Image.BILINEAR)
+        self.photoimg7=ImageTk.PhotoImage(img7)
+
+        b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
+        b1.place(x=1100,y=100,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Help Desk",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=300,width=200,height=40)
+
+        # Train data
+        img8=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\train.jpg")
+        img8=img8.resize((200,200),Image.BILINEAR)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+        
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
+        b1.place(x=200,y=370,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Train Data",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=570,width=200,height=40)
+
+        # Photos
+        img9=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\phot.jpg")
+        img9=img9.resize((200,200),Image.BILINEAR)
+        self.photoimg9=ImageTk.PhotoImage(img9)
+        
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1.place(x=500,y=370,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Photos",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=570,width=200,height=40)
+
+        # Developer
+        img10=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\dev.jpg")
+        img10=img10.resize((200,200),Image.BILINEAR)
+        self.photoimg10=ImageTk.PhotoImage(img10)
+
+        b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
+        b1.place(x=800,y=370,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Developer",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=570,width=200,height=40)
+
+        # Exit
+        img11=Image.open(r"C:\Users\KIIT01\Face Recognition System\images\exit.jpg")
+        img11=img11.resize((200,200),Image.BILINEAR)
+        self.photoimg11=ImageTk.PhotoImage(img11)
+
+        b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
+        b1.place(x=1100,y=370,width=200,height=200)
+
+        b1_1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=570,width=200,height=40)
+
+        
+
+       
 
 
 if __name__=="__main__":
---
+++ student.py
@@ -0,0 +1,10 @@
+from tkinter import* # It is library used for making GUI application & software
+from tkinter import ttk # for stylying purposes
+from PIL import Image,ImageTk  # For opening and displaying image files 
+#Python Imaging Library (expansion of PIL) is the de facto image processing package for Python language
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1920x1080+0+0")
+        self.root.title=("Face Recognition System")
